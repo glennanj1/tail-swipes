6ab1ad267ad4c6a1843fde684ecec3b45be092c2
glennanj1 <glennan20@gmail.com>
2021-05-05 15:16:28 -0400

    added profile container and tested

--- src/ProfileContainer.js
+++ src/ProfileContainer.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Oz from './ozzy_lick.png'
 
-exports default class ProfileContainer extends React.Component {
+export default class ProfileContainer extends React.Component {
+
     render() {
         return (
-            
+            <Card className='mx-auto' border='primary' style={{ width: '18rem'}}>
+                <Card.Img className='mx-auto'  variant="top" src={this.props.image} />
+                <Card.Body>
+                    <Card.Title>{this.props.name}</Card.Title>
+                    <Card.Text>
+                        <h2>{this.props.info}</h2>
+                    </Card.Text>
+                    <Button variant="primary">Paw</Button><br />
+                    <Button variant="danger">No Paw</Button>
+                </Card.Body>
+             </Card>
         )
     }
 }
