77a73d56b55df1e597ee6543f753532dd8b37978
glennanj1 <glennan20@gmail.com>
2021-05-12 18:04:19 -0400

    imported navlink and added implemented it into bootstrap navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 export default class Navigation extends React.Component{
     render(){
         return(
             <Navbar collapseOnSelect bg='primary' variant="dark" expand="sm">
-              <Navbar.Brand  href="#home"><NavLink to="/" exact>Tail Swipes</NavLink></Navbar.Brand>
+              <NavLink to='/' exact ><Navbar.Brand>TailSwipes</Navbar.Brand></NavLink>
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="d-flex justify-content-end">
-                    <Nav.Link  href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Parks</Nav.Link>
-                    <Nav.Link href="#link">Matches</Nav.Link>
+                    <NavLink to='/messages' exact ><Nav.Link>Messages</Nav.Link></NavLink>
+                    <NavLink to='/about' exact ><Nav.Link>About</Nav.Link></NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
