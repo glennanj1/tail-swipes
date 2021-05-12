5bde8e5dbc56f2ee5858dc2d58da3f4628f43461
glennanj1 <glennan20@gmail.com>
2021-05-12 16:59:21 -0400

    imported components, redux, redux-thunk and react-router, rootreducer, and bootstrap styling, rendered router and paths

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
+import Messages from './components/Messages'
+import About from './components/About'
+import Navigation from './components/Navigation'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+import profileReducer from './reducers/profileReducer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+const store = createStore(profileReducer, applyMiddleware(thunk));
 
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Provider store={store}>
+      <Router>
+        <Navigation />
+        <Route exact path="/" component={App} />
+        <Route exact path="/messages" component={Messages} />
+        <Route exact path="/about" component={About} />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
