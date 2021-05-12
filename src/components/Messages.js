ec6ef81d903393af6422cfafd99d1f71ee60359b
glennanj1 <glennan20@gmail.com>
2021-05-12 17:00:36 -0400

    added messages class

---
+++ src/components/Messages.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+export default class Messages extends React.Component {
+
+    render() {
+        return (
+            <h2>Message Feature</h2>
+        )
+    }
+}
