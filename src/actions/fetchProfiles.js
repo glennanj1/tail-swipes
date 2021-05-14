478f43c0330c8fbf997cd5c84dab1d64f1e537e5
glennanj1 <glennan20@gmail.com>
2021-05-14 15:05:01 -0400

    added fetch action

--- src/actions/fetchProfiles.js
+++ src/actions/fetchProfiles.js
@@ -1,8 +1,7 @@
 export function fetchProfiles() {
+    console.log('fetch action')
     return (dispatch) => {
         dispatch({ type: 'LOADING_PROFILES' });
-        fetch ('http://localhost:3000/profiles')
-            .then(r => r.json())
-            .then(profiles => dispatch({ type: 'ADD_PROFILES', profiles }))
+        fetch ('http://localhost:3000/profiles').then(r => r.json()).then(ResponseJSON => {dispatch({ type: 'ADD_PROFILES', profiles: ResponseJSON.data })})
     }
 }
