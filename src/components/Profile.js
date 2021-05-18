c48a5816b5c03d28fe3465c091afcba144d20080
github username <github email address>
2021-05-18 12:57:45 -0400

    added styling and removed border

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,18 +3,13 @@ import Card from 'react-bootstrap/Card'
 import TinderCard from 'react-tinder-card'
 
 
-
-
 class Profile extends Component {
 
     render() {
         return (
         <TinderCard  preventSwipe={['up', 'down']}>
-                <Card key={this.props.id} className='mx-auto' border='primary' style={{ position: 'absolute', left: '50%', top: '50%'}}>
-                <Card.Img className='mx-auto'  variant="top" src={this.props.image} />
-                    <Card.Body>
-                        <Card.Title>{this.props.name}</Card.Title>
-                    </Card.Body>
+                <Card key={this.props.id} className='mx-auto'  style={{ bg: 'blue !important', borderRadius: '20px', position: 'absolute', left: '50vh', right: "50vh", top: '10vh'}}>
+                    <Card.Img className='mx-auto'  variant="top" src={this.props.image} style={{borderRadius: '20px'}}/> 
                 </Card> 
         </TinderCard>
         )}
