912d5fd92435b46ca1363cbf6794f8d2ada2d6ef
github username <github email address>
2021-05-18 13:34:42 -0400

    added image overlay

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,6 +10,9 @@ class Profile extends Component {
         <TinderCard  preventSwipe={['up', 'down']}>
                 <Card key={this.props.id} className='mx-auto'  style={{ bg: 'blue !important', borderRadius: '20px', position: 'absolute', left: '50vh', right: "50vh", top: '10vh'}}>
                     <Card.Img className='mx-auto'  variant="top" src={this.props.image} style={{borderRadius: '20px'}}/> 
+                    <Card.ImgOverlay>
+                        <Card.Title style={{position: 'absolute', bottom: '0', color: 'white', fontSize: '50px'}} >{this.props.name}</Card.Title>
+                    </Card.ImgOverlay>
                 </Card> 
         </TinderCard>
         )}
