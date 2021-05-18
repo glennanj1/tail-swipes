12c8215e858d2fec03f8ab83f1c95da408b83c07
github username <github email address>
2021-05-18 12:06:18 -0400

    fixing container to contain logic

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -1,43 +1,16 @@
 import React from 'react'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import TinderCard from 'react-tinder-card'
-import * as Icon from 'react-bootstrap-icons';
+import Profile from '../components/Profile'
 
 
 
 export default class ProfileContainer extends React.Component {
 
     loadProfiles = () => {
-        
-        const onSwipe = (direction) => {
-            console.log('You swiped: ' + direction)
-        }
-
         return this.props.profiles.map(p =>
-            <TinderCard onSwipe={onSwipe} preventSwipe={['up', 'down']}>
-                <Card key={p.id} className='mx-auto' border='primary' style={{ width: '18rem'}}>
-                <Card.Img className='mx-auto'  variant="top" src={p.attributes.image} />
-                <Card.Body>
-                    <Card.Title>{p.attributes.name}</Card.Title>
-            
-                    <Button variant="primary">Paw</Button>
-
-                    
-
-                    <Button variant="danger">No Paw</Button>
-                </Card.Body>
-                </Card> 
-            </TinderCard>
+            <Profile id={p.id} name={p.attributes.name} image={p.attributes.image} />
         )}
 
     render() {
-
-
-        return (     
-                <div>{this.loadProfiles()}</div>
-        )
+        return  <div>{this.loadProfiles()}</div>
     }
-
-    
 }
