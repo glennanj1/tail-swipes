0e3a6c1ef6144afaec027c4ddd5f51a39bc53b84
github username <github email address>
2021-05-18 13:58:00 -0400

    aliased link for navbar for proper routing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 export default class Navigation extends React.Component{
     render(){
         return(
             <Navbar collapseOnSelect bg='primary' variant="dark" expand="sm">
-              <NavLink to='/' exact ><Navbar.Brand>TailSwipes</Navbar.Brand></NavLink>
+              <Navbar.Brand as={Link} to='/' >TailSwipes</Navbar.Brand>
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="d-flex justify-content-end">
-                    <NavLink to='/messages' exact ><Nav.Link>Messages</Nav.Link></NavLink>
-                    <NavLink to='/about' exact ><Nav.Link>About</Nav.Link></NavLink>
+                        <Nav.Link as={Link} to='/messages'>Messages</Nav.Link>
+                        <Nav.Link as={Link} to='/about'>About</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
