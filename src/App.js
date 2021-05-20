3f4ead5f17f7e4ec0f35195690b4fc2403d39d7f
github username <github email address>
2021-05-19 23:29:12 -0400

    added user messaes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MessageContainer from './containers/MessageContainer';
 import ProfileContainer from './containers/ProfileContainer';
+import UserMessages from './containers/UserMessages';
 
 
 export default class App extends React.Component {
@@ -20,6 +21,7 @@ export default class App extends React.Component {
         <Switch>
           <Route exact path="/" component={ProfileContainer} />
           <Route exact path="/messages" component={MessageContainer} />
+          <Route exact path="/messages/:user" component={UserMessages} />
           <Route exact path="/park" component={Park} />
         </Switch>
       </Router>
