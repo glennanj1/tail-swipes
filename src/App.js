3d38cc0194e64ab9b97ab6a5cabe1be6a71fb99d
github username <github email address>
2021-05-19 21:57:13 -0400

    refactored app to hold browser router, redux and thunk

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
 import React from 'react';
-import ProfileContainer from './containers/ProfileContainer'
+import Park from './components/Park'
+import Navigation from './components/Navigation'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+import rootReducer from './reducers'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MessageContainer from './containers/MessageContainer';
+import ProfileContainer from './containers/ProfileContainer';
+import Profile from './components/Profile';
 
+const store = createStore( rootReducer, applyMiddleware(thunk));
 
 export default class App extends React.Component {
 
   render() {
   return (
-    <div className="App">
-      <ProfileContainer />
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+        <Navigation />
+          <Route exact path="/" component={ProfileContainer} />
+          <Route exact path="/messages" component={MessageContainer} />
+          <Route exact path="/park" component={Park} />
+        </Switch>
+      </Router>
+    </Provider>
   );
   }
 }
