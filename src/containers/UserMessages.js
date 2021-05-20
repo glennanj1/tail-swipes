2303810b2f9a8beef64b95f7022e5a8c430e459c
github username <github email address>
2021-05-19 23:30:39 -0400

    started user message feature

---
+++ src/containers/UserMessages.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'react-bootstrap/Image'
+
+
+export default class Messages extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Nested Message</h2>
+            </div>
+        )
+    }
+}
