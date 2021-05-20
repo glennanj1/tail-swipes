1d0bd08b6c445f3160141b68b472392e34d85de0
github username <github email address>
2021-05-20 01:42:29 -0400

    fixed styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -13,8 +13,8 @@ class Profile extends Component {
             }
         }
         return (
-        <TinderCard  onSwipe={onSwipe} preventSwipe={['up', 'down']}>
-                <Card className='mx-auto'  style={{ borderRadius: '20px', position: 'absolute', left: '50vh', right: "50vh", top: '10vh'}}>
+        <TinderCard style={{display: 'flex'}} onSwipe={onSwipe} preventSwipe={['up', 'down']}>
+                <Card className='mx-auto'  style={{ position: 'absolute', borderRadius: '20px', maxWidth: '80vw'}}>
                     <Card.Img className='mx-auto'  variant="top" src={this.props.image} style={{borderRadius: '20px'}}/> 
                     <Card.ImgOverlay>
                         <Card.Title style={{position: 'absolute', bottom: '0', color: 'white', fontSize: '50px'}} >{this.props.name}</Card.Title>
