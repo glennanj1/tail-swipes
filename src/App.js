7db4ef317998579d4fadc247a65124d6b46b333c
github username <github email address>
2021-05-19 22:48:49 -0400

    removed redux into index

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 import './App.css';
 import React from 'react';
 import Park from './components/Park'
-import Navigation from './components/Navigation'
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import {
   BrowserRouter as Router,
   Route,
   Switch
-} from "react-router-dom";
-import rootReducer from './reducers'
+} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MessageContainer from './containers/MessageContainer';
 import ProfileContainer from './containers/ProfileContainer';
-import Profile from './components/Profile';
 
-const store = createStore( rootReducer, applyMiddleware(thunk));
 
 export default class App extends React.Component {
 
   render() {
   return (
-    <Provider store={store}>
+    <div className="App">
       <Router>
         <Switch>
-        <Navigation />
           <Route exact path="/" component={ProfileContainer} />
           <Route exact path="/messages" component={MessageContainer} />
           <Route exact path="/park" component={Park} />
         </Switch>
       </Router>
-    </Provider>
+    </div>
   );
   }
 }
