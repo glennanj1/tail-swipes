4dd4a21567dc4a2b2bdd0fef8ac912a715b4cd44
github username <glennan20@gmail.com>
2021-05-25 14:04:03 -0400

    wrapped tinder card in div and added styling to position card correctly

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,22 +5,27 @@ import TinderCard from 'react-tinder-card'
 
 class Profile extends Component {
 
+
     render() {
         const onSwipe = (direction) => {
             console.log(direction);
             if (direction === 'right' && this.props.match) {
-                alert(`You matched with ${this.props.name}`)
+                alert(`You matched with ${this.props.name}`);
             }
         }
         return (
-        <TinderCard style={{display: 'flex'}} onSwipe={onSwipe} preventSwipe={['up', 'down']}>
-                <Card className='mx-auto'  style={{ position: 'absolute', borderRadius: '20px', maxWidth: '80vw'}}>
-                    <Card.Img className='mx-auto'  variant="top" src={this.props.image} style={{borderRadius: '20px'}}/> 
-                    <Card.ImgOverlay>
-                        <Card.Title style={{position: 'absolute', bottom: '0', color: 'white', fontSize: '50px'}} >{this.props.name}</Card.Title>
-                    </Card.ImgOverlay>
-                </Card> 
-        </TinderCard>
+            <div style={{position: 'absolute', marginTop: '15vh'}}>
+                <TinderCard style={{position: 'absolute'}} onSwipe={onSwipe} preventSwipe={['up', 'down']}>
+                    <div style={{position: 'relative', maxWidth: '80vw'}>
+                        <Card style={{borderRadius: '20px'}}>
+                            <Card.Img style={{borderRadius: '20px'}} variant="top" src={this.props.image} /> 
+                            <Card.ImgOverlay>
+                                <Card.Title style={{position: 'absolute', bottom: '0', color: 'white', fontSize: '50px'}} >{this.props.name}</Card.Title>
+                            </Card.ImgOverlay>
+                        </Card> 
+                    </div>
+                </TinderCard>
+            </div>
         )}
 }
 
