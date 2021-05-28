eee36b87e06bea7c1a41fd9c6e46788355f10c6f
github username <glennan20@gmail.com>
2021-05-28 15:58:12 -0400

    fixed card logic added back in tinder card, modal and state

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,57 @@
 import { Component } from "react";
+import { connect } from 'react-redux';
 import Card from 'react-bootstrap/Card'
+import TinderCard from 'react-tinder-card'
+import Popup from "../components/Modal";
+import { createMessage } from '../actions/fetchMessages'
+
+
 
 
 class Profile extends Component {
+
+    state = {
+        isOpen: false
+      };
+
+    openModal = () => { console.log(this);
+        this.setState({ isOpen: true });
+    }
+    closeModal = () => this.setState({ isOpen: false });
+
+
+    onSwipe = (direction) => {
+        console.log(direction);
+        if (direction === 'right' && this.props.match) {
+            console.log(this)
+            this.props.createMessage({ 
+                message: {
+                    name: this.props.name ,
+                    message: this.props.message,
+                    profile_id: this.props.id,
+                    image: this.props.image
+                }
+            });
+            this.openModal();
+
+        }
+    }
+
     render() {
         return (
                 <div style={{position: 'relative', maxWidth: '80vw'}}>
-                    <Card style={{borderRadius: '20px'}}>
-                        <Card.Img style={{borderRadius: '20px'}} variant="top" src={this.props.image} /> 
-                        <Card.ImgOverlay>
-                            <Card.Title style={{position: 'absolute', bottom: '0', color: 'white', fontSize: '50px'}} >{this.props.name}</Card.Title>
-                        </Card.ImgOverlay>
-                    </Card> 
+                    <Popup name={this.props.name} show={this.state.isOpen} onHide={this.closeModal} closeModal={this.closeModal}/>
+
+                    <TinderCard style={{position: 'absolute'}}  onSwipe={this.onSwipe} preventSwipe={['up', 'down']}>
+                        <Card style={{borderRadius: '20px'}}>
+                            <Card.Img style={{borderRadius: '20px'}} variant="top" src={this.props.image} /> 
+                            <Card.ImgOverlay>
+                                <Card.Title style={{position: 'absolute', bottom: '0', color: 'white', fontSize: '50px'}} >{this.props.name}</Card.Title>
+                            </Card.ImgOverlay>
+                        </Card> 
+                    </TinderCard>
                 </div>
                 )}
 }
-
-export default Profile
+  
+export default connect(null, { createMessage })(Profile);
