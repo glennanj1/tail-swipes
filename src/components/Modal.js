dfcc2401fcfc87ced7e19d1f12d69aa14f25aa93
github username <glennan20@gmail.com>
2021-05-28 15:57:34 -0400

    fixed link to and added name to modal body

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -10,9 +10,9 @@ export default class Popup extends Component {
                 <Modal.Header closeButton>
                 <Modal.Title>Match!</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you matched with x!</Modal.Body>
+                <Modal.Body>Woohoo, you matched with {this.props.name}!</Modal.Body>
                 <Modal.Footer>
-                <Link to="/messages/{this.props.name}" className="btn btn-primary">Message</Link>
+                <Link to={`/messages/${this.props.name}`} className="btn btn-primary">Message</Link>
                 <Button variant="secondary" onClick={this.props.closeModal}>Close</Button>
                 </Modal.Footer>
                 </Modal>
