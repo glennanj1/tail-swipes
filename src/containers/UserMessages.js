c6eb3b756a29d03334e88e8452610065e4cac6ea
github username <glennan20@gmail.com>
2021-05-28 15:59:02 -0400

    added message show page component and assigned props

--- src/containers/UserMessages.js
+++ src/containers/UserMessages.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
+import MessageShow from '../components/MessageShow'
+import { connect } from 'react-redux'
+import { fetchMessages } from '../actions/fetchMessages'
 
-export default class Messages extends React.Component {
+class Messages extends React.Component {
+
+    componentDidMount(){
+        debugger;
+    }
 
     render() {
         return (
             <div>
                 <Navigation />
-                <h2>Nested Message</h2>
+                <h2>Message Show Page</h2>
+                <MessageShow image={this.props.image} name={this.props.name} message={this.props.message} />
             </div>
         )
     }
 }
+
+const mapStateToProps = state => {
+    return {
+      messages: state.messages.messages,
+      loading: state.profiles.loading
+    }
+}
+
+export default connect(mapStateToProps, { fetchMessages } )(Messages)
