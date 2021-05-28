a58c96db71b7204b5d57640973c783f7fe312ed5
github username <glennan20@gmail.com>
2021-05-28 15:58:36 -0400

    put logic back into profile component

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -4,50 +4,29 @@ import { fetchProfiles } from '../actions/fetchProfiles'
 import { connect } from 'react-redux';
 import Spinner from 'react-bootstrap/Spinner'
 import Navigation from '../components/Navigation'
-import Popup from "../components/Modal";
-import TinderCard from 'react-tinder-card'
 
 
 
 class ProfileContainer extends React.Component {
     
-    state = {
-        isOpen: false
-      };
-
-    openModal = () => this.setState({ isOpen: true });
-    closeModal = () => this.setState({ isOpen: false });
-
     componentDidMount(){
         this.props.fetchProfiles()
-        console.log('fetching profiles')  
     }
 
     loadProfiles = () => {
-        console.log(this.props.loading)
         if(this.props.loading) {
             return (<Spinner animation="border" size='large' role="status" style={{position: 'absolute', top: '10vh', left: '50vh'}}>
                         <span className="sr-only">Loading...</span>
                     </Spinner>)
         } else {
             return this.props.data.map(p => 
-            <TinderCard style={{position: 'absolute'}} onSwipe={this.onSwipe} preventSwipe={['up', 'down']}>
-                <Profile key={p.id} name={p.attributes.name} image={p.attributes.image} match={p.attributes.match} />
-            </TinderCard>
+                <Profile key={p.id} id={p.id} name={p.attributes.name} image={p.attributes.image} match={p.attributes.match} onSwipe={this.onSwipe} />
         )}
-    }
-
-    onSwipe = (direction) => {
-        console.log(direction);
-        if (direction === 'right' && this.props.match) {
-            this.openModal();
-        }
     }
 
     render() {
         return (<div>
                     <Navigation />
-                    <Popup show={this.state.isOpen} onHide={this.closeModal} closeModal={this.closeModal}/>
                     <div style={{display: 'flex', justifyContent: 'center'}}>
                         <div style={{position: 'absolute', marginTop: '15vh'}}>
                             {this.loadProfiles()}
