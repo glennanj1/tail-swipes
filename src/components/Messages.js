aaff76756e7fe0b9b4d80316dd493d649e428f15
glennanj1 <glennan20@gmail.com>
2021-06-01 15:40:57 -0400

    removed unused props

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import Image from 'react-bootstrap/Image'
+import {Link} from 'react-router-dom'
 
 
 
 export default class Messages extends React.Component {
 
+   
+
+
     render() {
         return (
             <div>
-                <div>
-                    <h1>{this.props.name}</h1>
-                    <p>{this.props.message}</p>
-                </div>
-                <Image src={this.props.image} roundedCircle />
-                
+                <Link style={{color: 'inherit', textDecoration: 'none'}} to={{ pathname: `/messages/${this.props.name}`, state: { id: this.props.id }}} >
+                    <div>
+                        <h1>{this.props.name}</h1>
+                        <p>{this.props.message}</p>
+                    </div>
+                    <Image src={this.props.image} roundedCircle />  
+                </Link>  
             </div>
         )
     }
