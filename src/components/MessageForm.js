a42411954a95256c7c9cb1df51b33783835df7b2
john glennan <glennan20@gmail.com>
2021-06-01 17:09:00 -0400

    added messaged form

---
+++ src/components/MessageForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+export default class MessageShow extends Component {
+    render() {
+        return (
+            <Form onSubmit={this.props.handleSubmit} style={{position: 'absolute', width: '100vw', bottom: '10px'}}>
+                <Form.Group controlId="formBasicMessage">
+                    <Form.Label>Messages</Form.Label>
+                    <Form.Control type="message" placeholder="Enter Message" />
+                    <Form.Text className="text-muted">
+                    example catch phrase here
+                    </Form.Text>
+                </Form.Group>
+                {/* <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group> */}
+                <Button variant="primary" type="submit">
+                    Send
+                </Button>
+            </Form>
+        )
+    }
+}
