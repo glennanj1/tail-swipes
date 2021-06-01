7159cebcc57bc6f352253264d40ea350294d473c
glennanj1 <glennan20@gmail.com>
2021-06-01 15:40:03 -0400

    added get message action

--- src/actions/fetchMessages.js
+++ src/actions/fetchMessages.js
@@ -9,6 +9,7 @@ export function fetchMessages() {
 export const createMessage = (message) => {
     console.log('fetch messages')
     return (dispatch) => {
+        dispatch({ type: 'LOADING_MESSAGES' });
         console.log('creating new message from match')
         const config = {
             method: 'POST', 
@@ -21,6 +22,16 @@ export const createMessage = (message) => {
         fetch ('http://localhost:3000/messages', config)
         .then(r => r.json())
         .then(ResponseJSON => {dispatch({ type: 'NEW_MESSAGE', payload: ResponseJSON.data})})
+    }
+}
+export const getMessage = (messageId) => {
+    console.log('fetch messages')
+    return (dispatch) => {
+        dispatch({ type: 'LOADING_MESSAGES' });
+        console.log(messageId)
+        fetch (`http://localhost:3000/messages/${messageId}`)
+        .then(r => r.json())
+        .then(ResponseJSON => {dispatch({ type: 'GET_MESSAGE', messages: ResponseJSON.data})})
     }
 }
 
