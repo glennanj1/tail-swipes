68015d76e7053a775a8b1a94d68b326ed605bf49
glennanj1 <glennan20@gmail.com>
2021-06-01 09:49:16 -0400

    added delete profile action

--- src/actions/fetchProfiles.js
+++ src/actions/fetchProfiles.js
@@ -5,3 +5,21 @@ export function fetchProfiles() {
         fetch ('http://localhost:3000/profiles').then(r => r.json()).then(ResponseJSON => {dispatch({ type: 'ADD_PROFILES', profiles: ResponseJSON.data })})
     }
 }
+
+export function deleteProfile(profileId) {
+    console.log('deleting action')
+    return (dispatch) => {
+        const config = {
+            method: 'DELETE', 
+            headers: {
+                'Content-Type': 'application/json',
+                'Accepts': 'application/json'
+            },
+        }
+        fetch(`http://localhost:3000/profiles/${profileId}`, config).then(r => {
+            return(r) => {
+                dispatch({ type: 'DELETE_PROFILE' })
+            }
+    })
+  }
+}
