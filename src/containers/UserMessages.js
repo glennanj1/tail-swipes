3a33ab1b83791d89c50979ec560c6a9f1704500e
glennanj1 <glennan20@gmail.com>
2021-06-01 15:43:44 -0400

    abstracting redux into container

--- src/containers/UserMessages.js
+++ src/containers/UserMessages.js
@@ -1,31 +1,18 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
-import MessageShow from '../components/MessageShow'
-import { connect } from 'react-redux'
-import { fetchMessages } from '../actions/fetchMessages'
 
-class Messages extends React.Component {
 
-    componentDidMount(){
-        debugger;
-    }
+export default class Messages extends React.Component {
 
     render() {
         return (
             <div>
                 <Navigation />
-                <h2>Message Show Page</h2>
-                <MessageShow image={this.props.image} name={this.props.name} message={this.props.message} />
+                <h5>You Matched on ... </h5>
+                MessageForm Component
             </div>
         )
     }
 }
 
-const mapStateToProps = state => {
-    return {
-      messages: state.messages.messages,
-      loading: state.profiles.loading
-    }
-}
 
-export default connect(mapStateToProps, { fetchMessages } )(Messages)
