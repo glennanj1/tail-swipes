e20469d7d714b4f444ee9a34828d4e4b8da00d5d
john glennan <glennan20@gmail.com>
2021-06-01 17:09:28 -0400

    imported message form and added props

--- src/containers/UserMessages.js
+++ src/containers/UserMessages.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import Navigation from '../components/Navigation'
+import MessageForm from '../components/MessageForm'
 
 
 export default class Messages extends React.Component {
+
+    handleSubmit = e => {
+        e.preventDefault();
+        //action to send message 
+    }
 
     render() {
         return (
@@ -10,6 +16,7 @@ export default class Messages extends React.Component {
                 <Navigation />
                 <h5>You Matched on ... </h5>
                 MessageForm Component
+                <MessageForm handleSubmit={this.handleSubmit} />
             </div>
         )
     }
