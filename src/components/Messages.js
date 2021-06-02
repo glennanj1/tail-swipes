e80b178b67ec4e119884eda12e785b83a133401e
john glennan <glennan20@gmail.com>
2021-06-01 22:08:36 -0400

    match and name added to link state

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -12,7 +12,7 @@ export default class Messages extends React.Component {
     render() {
         return (
             <div>
-                <Link style={{color: 'inherit', textDecoration: 'none'}} to={{ pathname: `/messages/${this.props.name}`, state: { id: this.props.id }}} >
+                <Link style={{color: 'inherit', textDecoration: 'none'}} to={{ pathname: `/messages/${this.props.name}`, state: { id: this.props.id, name: this.props.name, match: this.props.match }}} >
                     <div>
                         <h1>{this.props.name}</h1>
                         <p>{this.props.message}</p>
