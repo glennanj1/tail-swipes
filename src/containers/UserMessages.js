c809d07440ad41ec82a1b28fc1881b27eac8c3f9
john glennan <glennan20@gmail.com>
2021-06-01 22:08:11 -0400

    added link state props

--- src/containers/UserMessages.js
+++ src/containers/UserMessages.js
@@ -5,17 +5,20 @@ import MessageForm from '../components/MessageForm'
 
 export default class Messages extends React.Component {
 
+    
+
     handleSubmit = e => {
         e.preventDefault();
         //action to send message 
     }
 
     render() {
+        console.log(this.props)
         return (
             <div>
                 <Navigation />
-                <h5>You Matched on ... </h5>
-                MessageForm Component
+                <h5>You Matched with {this.props.location.state.name} on {this.props.location.state.match}</h5>
+                <p>Message From: </p>
                 <MessageForm handleSubmit={this.handleSubmit} />
             </div>
         )
