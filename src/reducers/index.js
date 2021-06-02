c77c65f4095991425fc4ca29ab956f1bb88efea4
glennanj1 <glennan20@gmail.com>
2021-06-02 12:58:50 -0400

    added conversation reducer in combined reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import messagesReducer from './messagesReducer'
 import profileReducer from './profileReducer'
+import conversationReducer from './conversationReducer'
 
 export default combineReducers({
     messages: messagesReducer,
-    profiles: profileReducer
+    profiles: profileReducer,
+    conversations: conversationReducer
 })
