b602e4ead58b54eb13906b684d56434c1231795d
github username <glennan20@gmail.com>
2021-06-02 14:34:14 -0400

    imported bootstrap and added image for default user

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
-
+import { Container, Image, Row, Col } from 'react-bootstrap'
 
 export default class Conversations extends Component {
     render() {
         return (
-            <div>
-                <img src={this.props.image} alt='dog' />
+            <div style={{marginBottom: '200pt'}}>
+                <Container>
+                    <Row>
+                        <Col xs={6} md={4}>
+                            <Image src='https://placedog.net/85/90?random' roundedCircle />
+                        </Col>
+                    </Row>
+                </Container>
                 <div>
                     <h3>{this.props.name}</h3>
                     {this.props.chat}
