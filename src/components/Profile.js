1c571f709acd802e6b5bd57a8480e4185151c63d
glennanj1 <glennan20@gmail.com>
2021-06-03 00:34:40 -0400

    removed modal from profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,15 @@
 import { Component } from "react";
 import Card from 'react-bootstrap/Card'
 import TinderCard from 'react-tinder-card'
-import Popup from "../components/Modal";
+
 
 export default class Profile extends Component {
 
     render() {
         return (
                 <div style={{position: 'relative', maxWidth: '80vw'}}>
-                    <Popup name={this.props.name} show={this.props.isOpen} onHide={this.props.closeModal} closeModal={this.props.closeModal}/>
-                    <TinderCard style={{position: 'absolute'}}  onSwipe={this.props.onSwipe} preventSwipe={['up', 'down']}>
+                    <TinderCard style={{position: 'absolute'}}  onSwipe={(dir) => this.props.onSwipe(dir, this.props)} preventSwipe={['up', 'down']}>
+                        
                         <Card style={{borderRadius: '20px'}}>
                             <Card.Img style={{borderRadius: '20px'}} variant="top" src={this.props.image} /> 
                             <Card.ImgOverlay>
