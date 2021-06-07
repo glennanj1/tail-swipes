b734ce3a27ebb2929505b326088ff4e91876961f
github username <glennan20@gmail.com>
2021-06-06 20:12:32 -0400

    changed park container to place container

---
+++ src/containers/PlaceContainer.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Navigation from '../components/Navigation' 
+import PlaceForm from '../components/PlaceForm'
+import Places from '../components/Places'
+import { connect } from 'react-redux'
+import { fetchPlace } from '../actions/fetchPlace'
+
+
+class PlaceContainer extends React.Component {
+
+    state = {
+        value: ''
+    }
+
+    loadPlaces = () => {
+        console.log(this.props.data)
+        return this.props.data.map(place => <Places name={place.attributes.name} vicinity={place.attributes.vicinity} />)
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.fetchPlace(this.state.value)
+        this.setState({value: ''})
+    }
+
+    handleChange = e => {
+        this.setState({value: e.target.value})
+    }
+
+    render() {
+        return(
+            <div>
+                <Navigation />
+                <PlaceForm value={this.state.value} handleSubmit={this.handleSubmit} onChange={this.handleChange} />
+                {this.loadPlaces()}
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+      data: state.places.places,
+      loading: state.places.loading
+    }
+}
+  
+export default connect(mapStateToProps, { fetchPlace })(PlaceContainer);
