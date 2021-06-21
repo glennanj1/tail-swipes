2a4680d3d817dcba5caf5b9b6d47396b8f9dc2ba
John Glennan <glennan20@gmail.com>
2021-06-21 11:11:17 -0400

    updated backend (and tested locally) for cost efficiency on heroku

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import rootReducer from './reducers'
 
 const store = createStore( rootReducer, applyMiddleware(thunk));
 
-export const url = 'https://tailswipes.herokuapp.com'
+export const url = 'https://top-sports-book-rails.herokuapp.com'
 
 // console.log(store.getState())
 
