0164b5970bd092770de8729d602ad982fb371716
John Glennan <61764500+glennanj1@users.noreply.github.com>
2021-06-28 08:36:46 -0400

    Update index.js
    changed global url

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import rootReducer from './reducers'
 
 const store = createStore( rootReducer, applyMiddleware(thunk));
 
-export const url = 'https://top-sports-book-rails.herokuapp.com'
+export const url = 'https://topsportsbook.org'
 
 // console.log(store.getState())
 
