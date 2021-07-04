c9e4189c868153118a9d8a0fcfdc4ff3a152d4bf
John Glennan <61764500+glennanj1@users.noreply.github.com>
2021-07-04 19:43:39 -0400

    Update MessageForm.js

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -4,7 +4,7 @@ import { Form, Button } from 'react-bootstrap'
 export default class MessageShow extends Component {
     render() {
         return (
-            <Form onSubmit={this.props.handleSubmit} style={{position: 'fixed', width: '100vw', bottom: '50px'}}>
+            <Form onSubmit={this.props.handleSubmit} style={{position: 'fixed', width: '100vw', bottom: '150px'}}>
                 <Form.Group controlId="formBasicMessage">
                     <Form.Label>Messages</Form.Label>
                     <Form.Control type="text" value={this.props.value} onChange={this.props.onChange} placeholder="Enter Message" />
