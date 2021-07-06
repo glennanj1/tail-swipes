205dac3b199e1a77fe048df593ec961ade6267cc
John Glennan <glennan20@gmail.com>
2021-07-06 16:04:07 -0400

    adjusted name styling for messaging

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -3,17 +3,17 @@ import { Image} from 'react-bootstrap'
 
 export default class Conversations extends Component {
     render() {
-        let style;
-        if (this.props.name === '') {
-            style = {left: '0px'}
-        } else {
-            style = {right: '0px'}
-        }
+        let cname;
+        this.props.name === '' ? cname = 'messageRight' : cname = 'messageLeft'
+
+        let name;
+        this.props.name === '' ? name = 'You' : name = this.props.name
+         
         return (
-            <div style={style}>
+            <div className={cname}>
                 <Image style={{position: 'relative', width: '60px', height: '60px'}} src={this.props.image} alt='picture' roundedCircle />
                 <p>
-                    <h3>{this.props.name}</h3>
+                    <h4>{name}</h4>
                     <p>{this.props.chat}</p>
                 </p>
             </div>
