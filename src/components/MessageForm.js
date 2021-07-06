6d4641331c7dc4c91dce1cc4358374e2b5af1978
John Glennan <glennan20@gmail.com>
2021-07-06 16:04:20 -0400

    removed example catch phrase

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Form, Button } from 'react-bootstrap'
 
+//add api calls under messages to give random funny text
+
 export default class MessageShow extends Component {
     render() {
         return (
@@ -9,7 +11,6 @@ export default class MessageShow extends Component {
                     <Form.Label>Messages</Form.Label>
                     <Form.Control type="text" value={this.props.value} onChange={this.props.onChange} placeholder="Enter Message" />
                     <Form.Text className="text-muted">
-                    example catch phrase here
                     </Form.Text>
                 </Form.Group>
                 <Button variant="primary" type="submit">
