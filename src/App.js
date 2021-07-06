ba6ae885a19bbf4f70a395ba6945ba851611f06c
John Glennan <glennan20@gmail.com>
2021-07-06 13:14:04 -0400

    refactor implementing DRY in containers

--- src/App.js
+++ src/App.js
@@ -11,26 +11,30 @@ import ProfileContainer from './containers/ProfileContainer';
 import UserMessages from './containers/UserMessages';
 import PlaceContainer from './containers/PlaceContainer'
 import Counter from './components/Counter'
+import Navigation from './components/Navigation'
+
 
 export default class App extends React.Component {
 
   render() {
   return (
     <div id="main" className="App">
+      
       <div id="wrap">
         <Router>
-          <Switch>
-            <Route exact path="/" component={ProfileContainer} />
-            <Route exact path="/messages" component={MessageContainer} />
-            <Route exact path="/messages/:user" component={UserMessages} />
-            <Route exact path="/park" component={PlaceContainer} />
-            <Route exact path="/counter" component={Counter} />
-          </Switch>
+          <Navigation />
+            <Switch>
+              <Route exact path="/" component={ProfileContainer} />
+              <Route exact path="/messages" component={MessageContainer} />
+              <Route exact path="/messages/:user" component={UserMessages} />
+              <Route exact path="/park" component={PlaceContainer} />
+              <Route exact path="/counter" component={Counter} />
+            </Switch>
         </Router>
 
       </div>
       <footer id="footer" className="text-center text-lg-start bg-primary">
-        <div class="text-center p-4 text-white">
+        <div className="text-center p-4 text-white">
           © 2021 Copyright: <a className="text-reset fw-bold" href="https://johng.netlify.app/"> John Glennan</a>
       </div>
     </footer>
