d78721fc50b67ce2847affacaa5e3046cc9741d7
John Glennan <glennan20@gmail.com>
2021-07-08 21:49:45 -0400

    jsx fragment

--- src/components/PlaceForm.js
+++ src/components/PlaceForm.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react'
 export default class PlaceForm extends Component {
     render() {
         return (
-            <div>
+            <>
                 <form onSubmit={this.props.handleSubmit}>
                     <label>Zip Code</label>
                     <input onChange={this.props.onChange} value={this.props.value} type='text' placeholder='zipcode' />
                     <input type='submit' value='Submit' />
                 </form>
                 <h4 style={{color: 'red'}}>{this.props.zipError}</h4>
-            </div>
+            </>
         )
     }
 }
