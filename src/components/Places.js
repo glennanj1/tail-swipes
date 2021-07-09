7761195eb3744c67fb77442ca31a8e7ee7da01b4
John Glennan <glennan20@gmail.com>
2021-07-08 21:49:54 -0400

    jsx fragment

--- src/components/Places.js
+++ src/components/Places.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 export default class Places extends Component {
     render() {
         return (
-            <div>
+            <>
                 <h2>{this.props.name}</h2>
                 <h3>{this.props.vicinity}</h3>
-            </div>
+            </>
         )
     }
 }
