1fbf702cd2d1d91918272a9a0a3b8b1d8f765820
John Glennan <glennan20@gmail.com>
2021-07-12 00:13:39 -0400

    added styling to zip code

--- src/components/Places.js
+++ src/components/Places.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import Figure from 'react-bootstrap/Figure'
 
 export default class Places extends Component {
     render() {
         return (
             <>
-                <h2>{this.props.name}</h2>
-                <h3>{this.props.vicinity}</h3>
+                <Figure className='figureContainer'>
+                    <Figure.Image
+                        width={85}
+                        height={90}
+                        alt="171x180"
+                        src={this.props.photo}
+                    />
+                     <h1>{this.props.name}</h1>
+                    <Figure.Caption>
+                        <h3>Address: {this.props.vicinity}</h3>
+                        <h4>Rating: {this.props.rating} Stars</h4>
+                    </Figure.Caption>
+                </Figure>
             </>
         )
     }
