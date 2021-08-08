9aee362152f40484a66cf4854fc946e02d0e997e
John Glennan <glennan20@gmail.com>
2021-08-07 23:40:09 -0400

    image tag fix

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,7 +10,7 @@ export default class Profile extends Component {
                 <div className='profileOuter'>
                     <TinderCard  onSwipe={(dir) => this.props.onSwipe(dir, this.props)} preventSwipe={['up', 'down']}>
                         <Card className='profileInner'>
-                            <Card.Img className='cardImage' variant="top" src={this.props.image} /> 
+                            <Card.Img className='cardImage' width="400" height="400" variant="top" src={this.props.image} /> 
                             <Card.ImgOverlay>
                                 <Card.Title className='cardTitle'>{this.props.name}</Card.Title>
                             </Card.ImgOverlay>
